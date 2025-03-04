14e8376a3738a7ef3be648363293b89b95acb8b0
Juaa6o6 <owo__omo@naver.com>
2025-03-04 23:06:07 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,12 +1,12 @@
 score = int(input())
 
-if 90 <= score <= 100 :
+if 90 <= score :
     print("A")
-elif 80 <= score < 90 :
+elif 80 <= score :
     print("B")
-elif 70 <= score < 80 :
+elif 70 <= score :
     print("C")
-elif 60 <= score < 70 :
+elif 60 <= score :
     print("D")
 else :
     print("F")
