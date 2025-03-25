9593650c9d85cfbbf45fbea62c9e1c2f416ad023
juaa6o6 <owo__omo@naver.com>
2025-03-26 00:08:47 +0900

    Update

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def math(A, B):
-    h = (A+B)*(A-B)
-    return str(h)
-
-A, B = map(int, sys.stdin.readline().split())
-sys.stdout.write(math(A, B))
--- 백준/Bronze/2475. 검증수/검증수.py
+++
@@ -1,7 +0,0 @@
-def koi(num):
-    a = 0
-    for i in num:
-        a += i**2
-    return a%10
-
-print(koi(list(map(int, input().split()))))
