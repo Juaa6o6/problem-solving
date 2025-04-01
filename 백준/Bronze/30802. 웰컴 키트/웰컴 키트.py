a057a55e09b4347f283ba18b4badd2c1de821891
Juaa6o6 <owo__omo@naver.com>
2025-04-01 17:19:30 +0900

    [Bronze III] Title: 웰컴 키트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -1,14 +1,13 @@
-import sys
-read = sys.stdin.readline
-N = int(input())
-size = list(map(int, read().split()))
-T, P = map(int, read().split())
-t_count = []
+n = int(input())
+size = list(map(int, input().split()))
+t, p = map(int, input().split())
+t_count = 0
 for i in size:
-    t = i//T
-    t_each = i%T
-    if 0 < t_each < T:
-        t += 1
-    t_count.append(t)
-print(sum(t_count))
-print((N//P), (N%P))
+    if i == 0:
+        continue
+    t_count += i//t
+    t_each = i%t
+    if t_each != 0:
+        t_count += 1
+print(t_count)
+print((n//p), (n%p))
