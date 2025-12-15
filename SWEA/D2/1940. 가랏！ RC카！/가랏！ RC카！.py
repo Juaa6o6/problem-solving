d61c6bb259952e518e8fbdd64ed0b9b392c0bc2a
Park Ju Hyeong <owo__omo@naver.com>
2025-12-15 18:05:05 +0900

    update code

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,3 +1,17 @@
+'''
+command 가속값
+
+command
+= 1 -> +
+= 2 -> -
+= 0 -> 속도 유지
+
+1 2 -> + 2
+1 1 -> + 1
+2 1 -> - 1
+2 2 -> - 2
+'''
+
 T = int(input())
 
 for tc in range(1, T+1):
@@ -8,20 +22,13 @@ for tc in range(1, T+1):
     for _ in range(num):
         lst = list(map(int, input().split()))
 
-        if lst == [1, 2]:
-            now += 2
-        elif lst == [1, 1]:
-            now += 1
-        elif lst == [2, 1]:
-            now -= 1
+        if lst[0] == 1:
+            now += lst[1]
+
+        elif lst[0] == 2:
+            now -= lst[1]
             if now < 0:
                 now = 0
-        elif lst == [2, 2]:
-            now -= 2
-            if now < 0:
-                now = 0
-        elif lst == [0]:
-            now = now
 
         speed += now
 
