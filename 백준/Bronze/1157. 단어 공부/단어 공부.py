c8fce7b7f1ee5ebc7529c5570135dfbed184397f
JuHyeong Park <owo__omo@naver.com>
2026-01-14 23:56:04 +0900

    [Bronze I] Title: 단어 공부, Time: 204 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -5,16 +5,7 @@ for w in st:
     num = ord(w)
     lst[num] += 1
 
-max_lst = []
-max_rst = max(lst)
-
-for idx, v in enumerate(lst):
-    if v != 0:
-        if max_rst <= v:
-            max_rst = v
-            max_lst.append(idx)
-
-if len(max_lst) > 1:
-    print('?')
+if lst.count(max(lst)) >= 2:
+    print("?")
 else:
-    print(chr(max_lst[0]))
+    print(chr(lst.index(max(lst))))
