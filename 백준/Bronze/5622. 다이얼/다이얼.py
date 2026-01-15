0080327a86bb5d409be775a84bdd598f94c6f19b
JuHyeong Park <owo__omo@naver.com>
2026-01-16 07:20:11 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,21 +1,11 @@
-dial = input()
-total = 0
-for w in dial:
-    if w in ['A', 'B', 'C']:
-        total += 3
-    elif w in ['D', 'E', 'F']:
-        total += 4
-    elif w in ['G', 'H', 'I']:
-        total += 5
-    elif w in ['J', 'K', 'L']:
-        total += 6
-    elif w in ['M', 'N', 'O']:
-        total += 7
-    elif w in ['P', 'Q', 'R', 'S']:
-        total += 8
-    elif w in ['T', 'U', 'V']:
-        total += 9
-    elif w in ['W', 'X', 'Y', 'Z']:
-        total += 10
+word = input()
+dial_groups = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+total_time = 0
 
-print(total)
+for w in word:
+    for i, group in enumerate(dial_groups):
+        if w in group:
+            total_time += i + 3
+            break
+
+print(total_time)
