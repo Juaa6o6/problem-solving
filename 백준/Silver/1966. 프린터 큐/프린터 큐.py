866faba641b881851b471cae757aa2fe9b64ef1c
JuHyeong Park <owo__omo@naver.com>
2026-01-25 16:24:34 +0900

    [Silver III] Title: 프린터 큐, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,26 @@
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+
+    paper = list(map(int, input().split()))
+    Max = max(paper)
+    paper = [(v, 0) for v in paper]
+    paper[m] = (paper[m][0], 1)
+
+    cnt = 0
+
+    while n:
+        num, t = paper.pop(0)
+        if num == Max and t:
+            cnt += 1
+            print(cnt)
+            break
+        else:
+            if num != Max:
+                paper.append((num, t))
+            else:
+                cnt += 1
+                n -= 1
+                Max = max(paper)
+                Max = Max[0]
