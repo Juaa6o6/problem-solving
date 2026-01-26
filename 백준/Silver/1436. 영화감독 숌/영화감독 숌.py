944faf55db9df89976bcfaab75f3c9aa019b9999
JuHyeong Park <owo__omo@naver.com>
2026-01-27 01:02:17 +0900

    [Silver V] Title: 영화감독 숌, Time: 572 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,14 +1,18 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
 
-num = 666
-count = 0
+n = int(input())
+
+number = 666
+cnt = 0
 
 while True:
-    if '666' in str(num):
-        count += 1
-    
-    if count == N:
-        print(num)
-        break
 
-    num += 1
+    if '666' in str(number):
+        cnt += 1
+
+        if cnt == n:
+            print(number)
+            break
+
+    number += 1
