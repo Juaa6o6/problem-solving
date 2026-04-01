17b8a60649806b0ba001fcbb83f78384585c6da2
JuHyeong Park <owo__omo@naver.com>
2026-04-02 00:08:27 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,19 +1,10 @@
 n = int(input())
-file = [input() for _ in range(n)]
-wcnt = len(file[0])
-search = ''
-if n == 1:
-    print(file[0])
-else:
-    for i in range(wcnt):
-        flag = True
-        for j in range(n-1):
-            if file[j][i] != file[j+1][i]:
-                search += '?'
-                flag = False
-                break
+first = list(input())
 
-        if flag:
-            search += file[j][i]
-            
-print(search)
+for _ in range(n-1):
+    word = input()
+    for i in range(len(word)):
+        if word[i] != first[i]:
+            first[i] = '?'
+
+print("".join(first))   
