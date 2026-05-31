947c6d1089fe9c9eb76aa8ce58dacbb37a8df086
JuHyeong Park <owo__omo@naver.com>
2026-06-01 00:09:25 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,4 +1,3 @@
 def solution(n):
     a = [q for q in range(1, int(n**0.5)+1) if n % q == 0]
-    b = [n // Q for Q in reversed(a) if Q != n // Q]
-    return a + b
+    return a + [n // Q for Q in reversed(a) if Q != n // Q]
